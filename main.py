e8fb29af2f02da1023656a8c0cfa886ac908efe2
Meetoaz Bhardwaj <bmeetoaz@gmail.com>
2023-03-02 09:55:41 +0530

    Update main.py
    Remove meow_gif.py due to links being saved in a separate module. Merged into one file and exploring alternative options such as using the Giphy API to avoid manual link saving.

--- main.py
+++ main.py
@@ -1,19 +1,51 @@
 import telebot  # pip install python-telegram-bot
-import meowgif  # to access gif link --> will find an alternative to it so anyone can add gifs
 import time
+import random
+
+meowGifs = [
+            'https://media.giphy.com/media/K1tgb1IUeBOgw/giphy.gif',
+            'https://media.giphy.com/media/xycr7jVnY8Z6U/giphy.gif',
+            'https://media.giphy.com/media/kBuMyaqdTIofe/giphy.gif',
+            'https://media.giphy.com/media/EoZfDAKYPoltS/giphy.gif',
+            'https://media.giphy.com/media/3o6gEdCJ0CgzXCW5Jm/giphy.gif',
+            'https://media.giphy.com/media/NQK1Vdx8E3gQ0/giphy.gif',
+            'https://media.giphy.com/media/xTiN0m4Q3VbqSJsTcc/giphy.gif',
+            'https://media.giphy.com/media/7KwCQcpVaMvGo/giphy.gif',
+            'https://media.giphy.com/media/ojiIqU9bu5u8/giphy.gif',
+            'https://media.giphy.com/media/rEp432aWc878xYjJ1C/giphy.gif',
+            'https://media.giphy.com/media/mo2SzgcelRCLMA1SSy/giphy.gif',
+            'https://media.giphy.com/media/NbhiwA0C8THIv8KvG5/giphy.gif',
+            'https://media.giphy.com/media/2nGGvhRuT31rA6SJr3/giphy.gif',
+            'https://media.giphy.com/media/JwNf393LMR5C/giphy.gif',
+            'https://media.giphy.com/media/hjLodVpYjLcwo/giphy.gif',
+            'https://media.giphy.com/media/hjLodVpYjLcwo/giphy.gif',
+            'https://media.giphy.com/media/3oz8y0fttuh21eUQOk/giphy.gif',
+            'https://media.giphy.com/media/LLx3PCNiEidsNQ58Wj/giphy.gif'
+            'https://media.giphy.com/media/ojiIqU9bu5u8/giphy.gif',
+            'https://media.giphy.com/media/Mc3yOYQDoXCj6/giphy.gif',
+            'https://media.giphy.com/media/l2JJDdD7cv4xdGGis/giphy.gif',
+            'https://media.giphy.com/media/UZz8BikCqfYzK/giphy.gif',
+            'https://media.giphy.com/media/oGP14S1XBi4yk/giphy.gif',
+            'https://media.giphy.com/media/kBuMyaqdTIofe/giphy.gif',
+            'https://media.giphy.com/media/109TWWxRddcIEg/giphy.gif',
+            'https://media.giphy.com/media/BRwgvFM9BfP8c/giphy.gif',
+            ]
+
+def giveMeowGif():
+    return random.choice(meowGifs)
 
 
 help1 = '''
-here are the commands and inline texts 
-you can use: 
+here are the commands and inline texts
+you can use:
 \hello
 \meow
 on
-bot senses inline texts such as 
-dance meow money 
+bot senses inline texts such as
+dance meow money
 
 we are making our bot smarter and you can help
-by dropping your suggestions, ideas over 
+by dropping your suggestions, ideas over
 @TheMeowChatBot_Team :)
 Thank you!
 🦋🍦🌻🖤
@@ -24,23 +56,23 @@ API_KEY = '#############################'
 bot = telebot.TeleBot(API_KEY)
 
 start_text = '''
-Get ready to bring some furry fun to your Telegram chats with 
-our meow bot ! Simply mention the word "meow", and our bot will 
-instantly send a cute cat GIF to brighten up your conversation. 
-But that's not all - our bot has a few surprises in store for you. 
-Try "dance"..."hey" for a playful greeting that will make you feel 
-right at home.There are many more surprises waiting for you, 
-so get ready to discover them all. With the Meow Bot, 
-you'll never have a dull moment in your chats again. 
+Get ready to bring some furry fun to your Telegram chats with
+our meow bot ! Simply mention the word "meow", and our bot will
+instantly send a cute cat GIF to brighten up your conversation.
+But that's not all - our bot has a few surprises in store for you.
+Try "dance"..."hey" for a playful greeting that will make you feel
+right at home.There are many more surprises waiting for you,
+so get ready to discover them all. With the Meow Bot,
+you'll never have a dull moment in your chats again.
 
 use /help for more information
 
-Thanks for adding meow bot, 
+Thanks for adding meow bot,
 Cheers to all the furry fun to your conversations!
 🦋🍦🌻🖤
 
 note: if you want to learn how to make a bot like this, help us make
-this bot better, check out the Github repository created for this bot 
+this bot better, check out the Github repository created for this bot
 at https://github.com/MeetoazBhardwaj/telegram-bot-python-TheMeowCatBot
 Thank you!
 '''
@@ -59,7 +91,7 @@ def hello(message):
 
 @bot.message_handler(commands=['meow'])
 def gif(message):
-    bot.send_animation(message.chat.id, meowgif.giveMeowGif())
+    bot.send_animation(message.chat.id, giveMeowGif())
 
 
 @bot.message_handler(commands=['help'])
@@ -90,15 +122,13 @@ def get_text_messages(message):
     elif 'fuck' in message.text.lower():
         bot.send_message(message.chat.id, username_text('used a bad word, me hurt :('))
     elif 'meow' in message.text.lower():
-        bot.send_animation(message.chat.id, meowgif.giveMeowGif())
+        bot.send_animation(message.chat.id, giveMeowGif())
     elif 'sing' in message.text.lower():
         bot.send_message(message.chat.id, username_text("sings meow-meow 🎶"))
     elif 'again' in message.text.lower():
         bot.send_message(message.chat.id, username_text("again? 🙄"))
     elif 'exam' in message.text.lower():
         bot.send_message(message.chat.id, username_text("ʕっ•ᴥ•ʔっ You got this!! 🐝🎶"))
-
-
 
 print('Bot Working . . .')
 while True:
